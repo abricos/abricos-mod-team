92d30c513857e1005861c12c2121a026add5d836
Alexander Kuzmin <roosit@abricos.org>
2013-10-10 19:06:59 +0400

    добавлен универсальный редактор участника

---
+++ js/membereditor.js
@@ -0,0 +1,391 @@
+/*
+@package Abricos
+@license http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE.php
+*/
+
+var Component = new Brick.Component();
+Component.requires = {
+	mod:[
+		{name: 'uprofile', files: ['users.js']},
+		{name: 'team', files: ['editor.js']}
+	]
+};
+Component.entryPoint = function(NS){
+	
+	var Dom = YAHOO.util.Dom,
+		E = YAHOO.util.Event,
+		L = YAHOO.lang;
+	
+	var UID = Brick.env.user.id;
+	var buildTemplate = this.buildTemplate;
+
+	var MemberEditorWidget = function(container, team, member, callback){
+		MemberEditorWidget.superclass.constructor.call(this, container, {
+			'buildTemplate': buildTemplate, 'tnames': 'widget' 
+		}, team, member, callback);
+	};
+	YAHOO.extend(MemberEditorWidget, Brick.mod.widget.Widget, {
+		init: function(team, member, callback){
+			this.team = team;
+			this.member = member;
+			this.callback = callback;
+			this.inviteWidget = null;
+			this._isVirtual = false;
+		},
+		buildTData: function(team, member){
+			return {'cledst':member.id==0?'edstnew': 'edstedit'};
+		},
+		onLoad: function(team, member){
+			if (member.id == 0){
+				var __self = this;
+				this.inviteWidget = new NS.MemberInviteWidget(this.gel('invite'), team, 
+					function(eml){ },
+					function(eml, user, femp){
+						__self.render();
+					}
+				);
+			}
+		},
+		onClick: function(el, tp){
+			this.clearError();
+
+			switch(el.id){
+			case tp['bskip']: this.setVirtual(); return true;
+			
+			case tp['bcreate']:
+			case tp['bsave']: this.save(); return true;
+			case tp['bcancel']: this.cancel(); return true;
+			}
+			return false;
+		},
+		setVirtual: function(){
+			this._isVirtual = true;
+			this.render();
+		},
+		render: function(){
+			this.elHide('loading');
+			this.elShow('editor');
+			
+			var u = Brick.env.user;
+			if (u.firstname.length == 0 || u.lastname.length == 0){
+				var __self = this;
+				var editor = new NS.MyNameEditorWidget(this.gel('mynameeditor'), this.team, function(){
+					editor.destroy();
+					__self.render();
+				});
+				this.elHide('profileok');
+				return;
+			}
+			
+			this.elShow('profileok');
+			this.elHide('empinfo');
+			
+			var member = this.member;
+			
+			this.elDisable('userfname,userlname');
+			
+			if (member.id == 0){ // добавление сотрудника (приглашение)
+				if (!this._isVirtual){
+					var inv = this.inviteWidget.getValue();
+					
+					if (L.isNull(inv)){ // не осуществлен поиск пользователя по емайл 
+						return; 
+					}
+					
+					if (!L.isNull(inv) && !L.isNull(inv['member'])){
+						return;
+					}
+					
+					if (this.inviteWidget.availableInvite()>0){
+						this.elEnable('bcreate');
+						this.elHide('bskip');
+					}
+					this.elShow('empinfo');
+					
+					user = inv['user'];
+					if (L.isNull(user)){ // новый пользователь 
+						this.elEnable('userfname,userlname');
+						return; 
+					}
+					
+					this.elSetValue({
+						'userfname': user['fnm'],
+						'userlname': user['lnm']
+					});
+					if (user['id'] == UID){
+						this.elEnable('userfname,userlname');
+					}
+				}else{
+					this.elHide('bskip,invite');
+					this.elShow('empinfo');
+					this.elEnable('bcreate,userfname,userlname');
+				}
+				
+			}else{ // редактирование существующего
+				this.elShow('empinfo');
+				
+				if (member.id == UID){
+					this.elEnable('userfname,userlname');
+				}
+				var user = NS.manager.users.get(member.id);
+				if (!L.isNull(user)){
+					this.elSetValue({
+						'userfname': user.firstName,
+						'userlname': user.lastName
+					});
+				}
+			}
+		},
+		clearError: function(){
+			for (var i=0;i<10;i++){
+				this.elHide('err'+i);
+			}
+		},
+		showError: function(num){
+			this.clearError();
+			this.elShow('err'+num);
+		},
+		cancel: function(){
+			NS.life(this.callback, 'cancel');
+		},
+		getSaveData: function(){
+			return sd = {
+				'id': this.member.id,
+				'vrt': this._isVirtual ? 1 : 0,
+				'teamid': this.team.id,
+				'fnm': this.gel('userfname').value,
+				'lnm': this.gel('userlname').value
+			};
+		},
+		save: function(){
+			var sd = this.getSaveData();
+
+			var inv = null;
+			if (this.member.id == 0 && !this._isVirtual){
+				inv = this.inviteWidget.getValue();
+				if (L.isNull(inv)){ return; }
+				sd['email'] = inv['email'];
+				
+				if (L.isNull(inv['user']) && 
+					(sd['fnm'].length == 0 || sd['lnm'].length == 0)){
+					this.showError(0);
+					return;
+					
+				}
+			}
+			
+			this.elHide('btns');
+			this.elShow('bloading');
+
+			var __self = this;
+			
+			this.team.manager.memberSave(this.team, sd, function(member){
+				__self.elShow('btns');
+				__self.elHide('bloading');
+				NS.life(__self.callback, 'save', member);
+			});
+		}
+	});
+	NS.MemberEditorWidget = MemberEditorWidget;	
+	
+	var MyNameEditorWidget = function(container, team, callback){
+		MyNameEditorWidget.superclass.constructor.call(this, container, {
+			'buildTemplate': buildTemplate, 'tnames': 'myname' 
+		}, team, callback);
+	};
+	YAHOO.extend(MyNameEditorWidget, Brick.mod.widget.Widget, {
+		init: function(team, callback){
+			this.team = team;
+			this.callback = callback;
+			this.editor = null;
+		},
+		onLoad: function(){
+			var u = Brick.env.user;
+			this.elSetValue({
+				'myfname': u.firstname,
+				'mylname': u.lastname
+			});
+			
+			var __self = this;
+			E.on(this.gel('fields'), 'keypress', function(e){
+				if (e.keyCode != 13){ return false; }
+				__self.saveMyProfile();
+				return true;
+			});
+		},
+		onClick: function(el, tp){
+			switch(el.id){
+			case tp['bmypsave']: this.saveMyProfile(); return true;
+			case tp['bmypcancel']: this.cancel(); return true;
+			}
+		},
+		cancel: function(){
+			NS.life(this.callback, 'cancel');
+		},
+		saveMyProfile: function(){
+			var __self = this;
+			this.elHide('mypbtns');
+			this.elShow('myloading');
+			
+			this.team.manager.ajax({
+				'do': 'mynamesave',
+				'firstname': this.gel('myfname').value,
+				'lastname': this.gel('mylname').value
+			}, function(d){
+				__self.elShow('mypbtns');
+				__self.elHide('myloading');
+
+				if (!L.isNull(d)){ 
+					var u = Brick.env.user;
+					u.firstname = d['firstname'];
+					u.lastname = d['lastname'];
+				}
+				
+				NS.life(__self.callback, 'save');
+			});
+		}
+	});
+	NS.MyNameEditorWidget = MyNameEditorWidget;
+	
+	var MemberInviteWidget = function(container, team, startCallback, finishCallback){
+		MemberInviteWidget.superclass.constructor.call(this, container, {
+			'buildTemplate': buildTemplate, 'tnames': 'invite' 
+		}, team, startCallback, finishCallback);
+	};
+	YAHOO.extend(MemberInviteWidget, Brick.mod.widget.Widget, {
+		init: function(team, startCallback, finishCallback){
+			this.team = team;
+			this.startCallback = startCallback;
+			this.finishCallback = finishCallback;
+			this.value = null;
+			this._isProcess = false;
+		},
+		availableInvite: function(){
+			var ucfg  = this.team.manager.userConfig;
+			if (ucfg['inviteWaitLimit'] == -1){
+				return 999;
+			}
+			return ucfg['inviteWaitLimit'] - ucfg['inviteWaitCount'];
+		},
+		buildTData: function(team){
+			return {
+				'invcnt': this.availableInvite(),
+				'waitcnt':  this.team.manager.userConfig['inviteWaitCount']
+			};
+		},
+		onLoad: function(){
+			var __self = this;
+			E.on(this.gel('email'), 'keyup', function(e){
+				__self.emailValidate();
+			});
+
+			this.userFind = new NS.UserFindByEmail(
+				function(eml){ __self._startCheckCallback(eml); },
+				function(eml, user){ __self._finishCheckCallback(eml, user); }
+			);
+		},
+		onClick: function(el, tp){
+			switch(el.id){
+			case tp['bfind']: this.find(); return true;
+			}
+		},
+		onEnter: function(el, tp){
+			switch(el.id){
+			case tp['email']: this.find(); return true;
+			}
+		},
+		find: function(){
+			if (this._isProcess){ return; }
+			this.userFind.find(this.gel('email').value);
+		},
+		emailValidate: function(){
+			if (this._isProcess){ return; }
+			if (NS.emailValidate(this.gel('email').value)){
+				this.gel('bfind').disabled = "";
+			}else{
+				this.gel('bfind').disabled = "disabled";
+			}
+		},
+		_startCheckCallback: function(eml){
+			if (this._isProcess){ return; }
+			this._isProcess = true;
+
+			this.value = null;
+			this.elDisable('bfind');
+			this.elShow('emlld');
+			this.gel('email').readonly = "readonly";
+			NS.life(this.startCallback, eml);
+		},
+		_finishCheckCallback: function(eml, user){
+			this._isProcess = false;
+			
+			for (var i=0;i<10;i++){
+				this.elSetHTML('resemail'+i, eml);
+			}
+			this.elEnable('bfind');
+			this.gel('email').readonly = "";
+			this.elHide('emlld,userfound,usernotfound,empfound,empnotfound');
+			
+			this.elShow('waitinfo');
+			var member = null;
+			
+			if (L.isNull(user)){
+				this.elShow('usernotfound');
+			}else{
+				this.elShow('userfound,next');
+				
+				member = this.team.memberList.get(user.id);
+				
+				if (L.isNull(member)){
+					this.elShow('empnotfound');
+					this.elSetValue({
+						'username': user['unm']
+					});
+				}else{
+					this.elShow('empfound');
+				}
+			}
+			this.value = {
+				'email': eml,
+				'user': user,
+				'member': member
+			};
+			NS.life(this.finishCallback, eml, user, member);
+		},
+		getValue: function(){
+			return this.value;
+		}
+	});
+	NS.MemberInviteWidget = MemberInviteWidget;
+	
+	var MemberRemovePanel = function(team, member, callback){
+		this.team = team;
+		this.member = member;
+		this.callback = callback;
+		MemberRemovePanel.superclass.constructor.call(this, {fixedcenter: true});
+	};
+	YAHOO.extend(MemberRemovePanel, Brick.widget.Dialog, {
+		initTemplate: function(){
+			return buildTemplate(this, 'removepanel').replace('removepanel');
+		},
+		onClick: function(el){
+			var tp = this._TId['removepanel'];
+			switch(el.id){
+			case tp['bcancel']: this.close(); return true;
+			case tp['bremove']: this.remove(); return true;
+			}
+			return false;
+		},
+		remove: function(){
+			var TM = this._TM, gel = function(n){ return  TM.getEl('removepanel.'+n); },
+				__self = this;
+			Dom.setStyle(gel('btns'), 'display', 'none');
+			Dom.setStyle(gel('bloading'), 'display', '');
+			NS.manager.memberRemove(this.team, this.member, function(){
+				__self.close();
+				NS.life(__self.callback);
+			});
+		}
+	});
+	NS.MemberRemovePanel = MemberRemovePanel;	
+};
